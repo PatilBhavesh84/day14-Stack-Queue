c3322299bd27e08601690a45e783f7a4cfb13702
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-26 20:45:00 +0530

    Added:Created a stack

--- src/com/BridgeLabz/Stack.java
+++ src/com/BridgeLabz/Stack.java
@@ -1,8 +1,46 @@
 package com.BridgeLabz;
-
+import java.util.Scanner;
 public class Stack {
+	int top;
+	int maxsize = 3;
+	int[] arr = new int[maxsize];
 
-	public static void main(String[] args) {
-		System.out.println("Welcome");
+
+	boolean isEmpty()
+	{
+		return (top < 0);
+	}
+	Stack()
+	{
+		top = -1;
+	}
+	boolean push (Scanner sc)
+	{
+		if(top == maxsize-1)
+		{
+			System.out.println("Overflow !!");
+			return false;
+		}
+		else
+		{
+			System.out.println("Enter Value");     
+			int val = sc.nextInt();
+			top++;
+			arr[top]=val;  
+			return true;
+		}
+	}
+
+	void display ()
+	{
+		System.out.println("Stack elements");
+		for(int i = top; i>=0;i--)
+		{
+			System.out.println(arr[i]);
+		}
 	}
 }
+
+
+
+
